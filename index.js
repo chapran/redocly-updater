278aad320fb5849122e08cf2cf0bc838e874c090
Andriy Chapran <andriy.chapran@eleks.com>
2021-09-14 15:13:13 +0300

    Add git uploader

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 const fs = require("fs").promises;
-
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 
+const { createBranch, commitAndPushUpdates } = require("./lib/git");
+
 async function update() {
-  const { p, v } = yargs(hideBin(process.argv)).argv;
-  const jsonPath = `${process.cwd()}/package.json`;
+  const { p, v, remoteName } = yargs(hideBin(process.argv)).argv;
+
+  await createBranch(p);
+  const jsonPath = `${process.cwd()}/package.json`; // TODO: change to regular npm i command
 
   const pckgJson = require(jsonPath);
 
@@ -17,6 +20,8 @@ async function update() {
   pckgJson.dependencies[p] = v;
 
   await fs.writeFile(jsonPath, JSON.stringify(pckgJson, null, 2));
+
+  await commitAndPushUpdates(p, v, remoteName)
 }
 
 update();
